d2d3a5cddcd755e2ab852215ffc8c4b7f3bbeba1
GabrielPila <gabriel.pilah@pucp.edu.pe>
2022-01-23 23:04:17 -0500

    Extractor Improved

--- api/main.py
+++ api/main.py
@@ -2,14 +2,11 @@ import requests
 import os
 import json
 import pandas as pd
-import csv
 import datetime
-import dateutil.parser
-import unicodedata
-import time
+from tqdm import tqdm
 
 from settings import BEARER_TOKEN
-from params import search_url, keyword, start_time, end_time, max_results
+from params import search_url, keyword, start_time, max_results, step_time, num_steps
 from utils import create_headers, create_url, connect_to_endpoint
 
 
@@ -17,9 +14,35 @@ from utils import create_headers, create_url, connect_to_endpoint
 bearer_token = BEARER_TOKEN
 headers = create_headers(bearer_token)
 
+start_time = pd.to_datetime(start_time)
 
-params = create_url(keyword, start_time,end_time, max_results)
-json_response = connect_to_endpoint(search_url, headers, params)
+if not os.path.exists('data'):
+    os.mkdir('data')
+if not os.path.exists('data_users'):
+    os.mkdir('data_users')
 
-print(list(json_response))
-# print(json.dumps(json_response, indent=4, sort_keys=True))
+for i in tqdm(list(range(num_steps))):
+    start = str(start_time + datetime.timedelta(minutes = i * step_time))
+    end = str(start_time + datetime.timedelta(minutes = (i+1) * step_time))
+
+    start = start.replace(" ", "T").replace("+00.00",".000Z")
+    end = end.replace(" ", "T").replace("+00.00",".000Z")
+
+    params = create_url(keyword, start, end, max_results)
+    json_response = connect_to_endpoint(search_url, headers, params)
+    timestamp = str(datetime.datetime.now())[:-4]
+    end_file = f'start_{start.split("+")[0]}_end_{end.split("+")[0]}'
+
+    data = pd.DataFrame(json_response['data'])
+    data['keyword'] = keyword
+    data['timestamp'] = timestamp
+    data.to_csv(f'data/dataTW_GP_{end_file}.csv', index=False)
+
+    users = pd.DataFrame(json_response['includes']['users'])
+    users['keyword'] = keyword
+    users['timestamp'] = timestamp
+    users.to_csv(f'data_users/usersTW_GP_{end_file}.csv', index=False)
+
+
+#params = create_url(keyword, start_time, end_time, max_results)
+#json_response = connect_to_endpoint(search_url, headers, params)
--- api/params.py
+++ api/params.py
@@ -1,5 +1,7 @@
 search_url = "https://api.twitter.com/2/tweets/search/recent" #Change to the endpoint you want to collect data from
-keyword = "repsol -is:retweet lang:es"
-start_time = "2022-01-19T00:00:00.000Z"
-end_time = "2022-01-23T00:00:00.000Z"
-max_results = 10
+keyword = "repsol -is:retweet lang:es" 
+start_time = "2022-01-19T00:00:00.000Z" # start of the extraction
+#end_time = "2022-01-23T00:00:00.000Z"
+step_time = 10 # minutes
+num_steps = 3 # number of extractions
+max_results = 10 # number of results extracted per request
