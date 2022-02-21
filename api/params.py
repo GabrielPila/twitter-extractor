b56f0e7f35b797240a910f14195b28d0aef4040c
GabrielPila <gabriel.pilah@pucp.edu.pe>
2022-02-21 08:52:59 -0500

    Added Reporte NLP

--- api/params.py
+++ api/params.py
@@ -1,6 +1,6 @@
 search_url = "https://api.twitter.com/2/tweets/search/recent" #Change to the endpoint you want to collect data from
-keyword = "@PedroCastilloTe -is:retweet lang:es" 
-start_time = "2022-02-13T21:50:00.000Z" # start of the extraction
+keyword = "Malcricarmen -is:retweet lang:es" 
+start_time = "2022-02-20T20:10:00.000Z" # start of the extraction
 #start_time = "2022-02-10T09:00:00.000Z" # start of the extraction
 #end_time = "2022-01-23T00:00:00.000Z"
 step_time = 10 # minutes
